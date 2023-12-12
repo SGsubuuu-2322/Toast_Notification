f0d25e4136ec69edc1a49d8dea1817053e2ff5b4
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-12 23:48:47 +0530

    Added some scripts for basic setup in the script file...

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+const messages = [
+  "message-One",
+  "message-Two",
+  "message-Three",
+  "message-Four",
+];
+
+button.addEventListener("click", () => createNotifications());
+
+function createNotifications() {
+  console.log("123");
+}
